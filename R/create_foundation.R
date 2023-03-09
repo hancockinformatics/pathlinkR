1e465536b65124971a02f139b6ce364e94d29661
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-03-09 10:10:55 -0800

    Updated create_foundation function.

--- R/create_foundation.R
+++ R/create_foundation.R
@@ -1,1 +1,37 @@
-create_foundation <- function() {}
+#' create_foundation
+#'
+#' @param mat Matrix of distances between pathways (i.e. 0 means two pathways
+#'   are identical).
+#' @param cutoff Distance cutoff used to determine if two pathways should share
+#'   an edge.
+#'
+#' @return A tibble
+#' @export
+#'
+#' @import dplyr
+#' @import tibble
+#' @import tidyr
+#'
+#' @description From a n by n distance matrix, generate a three column tibble to
+#'   use in constructing a pathway network. The cutoff can be adjusted to have
+#'   more or fewer edges in the final network, depending on the number of
+#'   pathways involved.
+#'
+#' @references None.
+#'
+#' @seealso <https://www.github.com/travis-m-blimkie/tRavis>
+#'
+create_foundation <- function(mat, cutoff = 0.1) {
+
+  stopifnot(all(rownames(mat) == colnames(mat)))
+
+  mat_tibble <- mat %>%
+    as.data.frame() %>%
+    rownames_to_column("pathway_1") %>%
+    pivot_longer(-pathway_1, names_to = "pathway_2", values_to = "distance")
+
+  edge_table <- mat_tibble %>%
+    filter(distance < cutoff)
+
+  return(edge_table)
+}
