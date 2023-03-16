b9d84120ab37baa16ea422f8c72539b3dab994db
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-03-16 15:56:14 -0700

    Updated create_foundation, working through workflow_ex.

--- R/create_foundation.R
+++ R/create_foundation.R
@@ -4,7 +4,9 @@
 #'   are identical).
 #' @param max_distance Distance cutoff (less than or equal) used to determine if
 #'   two pathways should share an edge. Pairs with a distance of 0 are always
-#'   removed.
+#'   removed. One of `max_distance` or `prop_to_keep` must be provided.
+#' @param prop_to_keep Top proportion of pathway pairs to keep as edges. One of
+#'   `max_distance` or `prop_to_keep` must be provided.
 #'
 #' @return A tibble
 #' @export
@@ -13,26 +15,44 @@
 #' @import tibble
 #' @import tidyr
 #'
-#' @description From a n by n distance matrix, generate a three column tibble to
-#'   use in constructing a pathway network. The cutoff can be adjusted to have
-#'   more or fewer edges in the final network, depending on the number of
-#'   pathways involved.
+#' @description From a n by n distance matrix, generate a tibble to use in
+#'   constructing a pathway network. The cutoff can be adjusted to have more or
+#'   fewer edges in the final network, depending on the number of pathways
+#'   involved.
 #'
 #' @references None.
 #'
 #' @seealso <https://github.com/hancockinformatics/pathnet>
 #'
-create_foundation <- function(mat, max_distance = 0.1) {
+create_foundation <- function(mat, max_distance = NA, prop_to_keep = NA) {
 
   stopifnot(all(rownames(mat) == colnames(mat)))
 
   mat_tibble <- mat %>%
     as.data.frame() %>%
     rownames_to_column("pathway_1") %>%
-    pivot_longer(-pathway_1, names_to = "pathway_2", values_to = "distance")
+    pivot_longer(-pathway_1, names_to = "pathway_2", values_to = "distance") %>%
+    distinct() %>%
+    filter(distance != 0) %>%
+    arrange(distance)
 
-  edge_table <- mat_tibble %>%
-    filter(distance != 0, distance <= max_distance)
+  if (!is.na(max_distance)) {
+    edge_table <- filter(mat_tibble, distance <= 0.5)
+  } else if (!is.na(prop_to_keep)) {
+    edge_table <- slice_head(mat_tibble, prop = prop_to_keep)
+  }
 
-  return(edge_table)
+  anno_edge_table <- edge_table %>%
+    left_join(
+      distinct(select(sigora_database, pathway_id, pathway_name)),
+      by = c("pathway_1" = "pathway_id")
+    ) %>%
+    left_join(
+      distinct(select(sigora_database, pathway_id, pathway_name)),
+      by = c("pathway_2" = "pathway_id"),
+      suffix = c("_1", "_2")
+    ) %>%
+    relocate(contains("name"), distance)
+
+  return(anno_edge_table)
 }
