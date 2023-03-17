c1c798dc6eea1b6cf29389845dea983b25cee7de
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-03-17 12:34:01 -0700

    Working on version that only uses enriched pathways.

--- workflow_ex.R
+++ workflow_ex.R
@@ -5,34 +5,10 @@ library(ggraph)
 library(tidyverse)
 
 
-# Approaches --------------------------------------------------------------
-
-#' The way I see it, there are two options for how to construct these networks:
-#' Option A
-#' - Start with a distance matrix
-#' - Define two pathways as sharing an edge based on a maximum distance cutoff
-#' - Attach results from Sigora (direction, p-value, etc.)
-#' - Convert this table to a network object
-#'   - Remove subgraphs with no enriched nodes
-#' - Plot it
-#'
-#' Option B
-#' - Start with distance matrix
-#' - Turn this into a network, including basically all pathway pairs as sharing
-#'   an edge
-#' - Convert this into a network object (very large!)
-#' - Extract a subnetwork from this using the pathways from Sigora results as
-#'   the target nodes. Attach Sigora information during this process
-#' - Plot it
-
-
-# Option A ----------------------------------------------------------------
+# Example 1 ---------------------------------------------------------------
 
 # |- get_pathway_distances and create_foundation --------------------------
 
-# Using the pre-calculated Jaccard distances, filter with a distance of 0.5,
-# where any pair of pathways with distance < "max_distance" are considered
-# connected.
 dist_data <- get_pathway_distances(
   pathway_data = sigora_database,
   dist_method = "jaccard"
@@ -47,14 +23,14 @@ starting_pathways <- create_foundation(
 # |- create_pathnet -------------------------------------------------------
 
 pathways_as_network <- create_pathnet(
-  sigora_result = sigora_result_eg,
+  sigora_result = sigora_example_1,
   foundation = starting_pathways,
   trim = TRUE,
   trim_order = 2
 )
 
 
-# |- Plot it! -------------------------------------------------------------
+# |- plot -----------------------------------------------------------------
 
 ggraph(pathways_as_network, layout = "nicely") +
   geom_edge_link(aes(edge_width = log10(similarity)), alpha = 0.3) +
@@ -68,6 +44,60 @@ ggraph(pathways_as_network, layout = "nicely") +
     size = 6,
     repel = TRUE,
     alpha = 0.5,
+    max.overlaps = 3,
+    min.segment.length = 0
+  ) +
+  scale_edge_width(range = c(0.5, 2)) +
+  scale_size_continuous(range = c(4, 8)) +
+  scale_fill_discrete(na.value = "grey") +
+  theme_void()
+
+
+# Example 2 ---------------------------------------------------------------
+
+# Here we'll restrict the data using the pathways/genes from the Sigora results,
+# instead of using all pathways and layering our results on top
+sigora_example_2
+
+# Start by splitting the sigora genes, and adding the needed annotations from
+# sigora_database
+candidate_data <- sigora_example_2 %>%
+  select(pathway_id, genes) %>%
+  separate_rows(genes, sep = ";") %>%
+  left_join(sigora_database, by = c("pathway_id", "genes" = "Symbol")) %>%
+  relocate(pathway_id, Ensembl.Gene.ID, "Symbol" = genes, pathway_name) %>%
+  distinct()
+
+# Now that we have a smaller table in the same format as sigora_database, we can
+# construct the foundation of pathway interactions
+candidate_dist_data <- get_pathway_distances(
+  pathway_data = candidate_data,
+  dist_method = "jaccard"
+)
+
+candidate_starting_pathways <- create_foundation(
+  mat = candidate_dist_data,
+  prop_to_keep = 0.9
+)
+
+candidates_as_network <- create_pathnet(
+  sigora_result = sigora_example_2,
+  foundation = candidate_starting_pathways,
+  trim = FALSE,
+)
+
+ggraph(candidates_as_network, layout = "nicely") +
+  geom_edge_link(aes(edge_width = log10(similarity)), alpha = 0.2) +
+  geom_node_point(
+    aes(size = -log10(bonferroni), fill = direction),
+    pch = 21,
+    colour = "black"
+  ) +
+  geom_node_label(
+    aes(label = node_label),
+    size = 6,
+    repel = TRUE,
+    alpha = 0.75,
     max.overlaps = 3,
     min.segment.length = 0
   ) +
