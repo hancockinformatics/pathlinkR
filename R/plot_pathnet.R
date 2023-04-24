309372e52a264e73cd6554c3bb31250f9f959aae
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-04-24 10:11:39 -0700

    Started defining what the plotting function needs to do.

--- R/plot_pathnet.R
+++ R/plot_pathnet.R
@@ -1,1 +1,15 @@
-plot_pathnet <- function() {}
+plot_pathnet <- function() {
+
+  #' - Create wrapped version of pathway name to label nodes
+  #' - Create "dummy" column so all nodes have coloured outline, but only
+  #'   enriched nodes have the same fill colour
+
+  # mutate(
+  #   pathway_name_1_wrap = map_chr(
+  #     as.character(pathway_name_1),
+  #     ~str_wrap(.x, width = 20)
+  #   ),
+  #   pathway_name_1_wrap = str_replace(pathway_name_1_wrap, "^$", NA_character_)
+  # )
+
+}
