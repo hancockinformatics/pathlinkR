da54811e9d5138bb94ed946eb7906facc982d916
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-04-24 11:28:09 -0700

    Added simple version of pathnet plotting function. Fixed up some documentation issues.

--- R/plot_pathnet.R
+++ R/plot_pathnet.R
@@ -1,15 +1,91 @@
-plot_pathnet <- function() {
+#' plot_pathnet
+#'
+#' @param network Tidygraph network object, output from `create_pathnet`. See
+#'  Details for specific requirements.
+#' @param net_layout Desired layout for the network visualization. Defaults to
+#'   "nicely", but supports any method found in `?layout_tbl_graph_igraph`
+#' @param edge_alpha Alpha value for edges
+#'
+#' @return An object of class "gg"
+#' @export
+#'
+#' @import ggplot2
+#' @import ggraph
+#' @import dplyr
+#'
+#' @description Plots the network object
+#'
+#' @references None.
+#'
+#' @seealso <https://github.com/hancockinformatics/pathnet>
+#'
+plot_pathnet <- function(network, net_layout = "nicely", edge_alpha = 0.67) {
 
-  #' - Create wrapped version of pathway name to label nodes
-  #' - Create "dummy" column so all nodes have coloured outline, but only
-  #'   enriched nodes have the same fill colour
+  # Check column names for both nodes and edges
+  stopifnot(all(
+    c(
+      "pathway_name_1",
+      "description",
+      "grouped_pathway",
+      "bonferroni"
+    ) %in% colnames(as_tibble(network))
+  ))
 
-  # mutate(
-  #   pathway_name_1_wrap = map_chr(
-  #     as.character(pathway_name_1),
-  #     ~str_wrap(.x, width = 20)
-  #   ),
-  #   pathway_name_1_wrap = str_replace(pathway_name_1_wrap, "^$", NA_character_)
-  # )
+  stopifnot(all(
+    "similarity" %in% colnames(as_tibble(tidygraph::activate(network, "edges")))
+  ))
 
+  network <- network %>%
+    mutate(
+      node_fill = if_else(!is.na(description), grouped_pathway, NA_character_),
+      pathway_name_1_wrap = map_chr(
+        as.character(pathway_name_1),
+        ~str_wrap(.x, width = 20)
+      ),
+      pathway_name_1_wrap = str_replace(pathway_name_1_wrap, "^$", NA_character_)
+    )
+
+  ggraph(network, layout = net_layout) +
+    geom_edge_link(aes(edge_width = log10(similarity)), alpha = edge_alpha) +
+    geom_node_point(
+      aes(size = -log10(bonferroni), fill = node_fill, colour = grouped_pathway),
+      pch = 21,
+      stroke = 1.5
+    ) +
+    geom_node_label(
+      aes(label = pathway_name_1_wrap),
+      repel = TRUE,
+      alpha = 0.67,
+      min.segment.length = 0,
+      max.overlaps = 6
+    ) +
+    scale_edge_width(range = c(0.33, 3), name = "Similarity") +
+    scale_size_continuous(
+      labels = scales::label_math(expr = 10^-~.x),
+      range = c(4, 8)
+    ) +
+    scale_colour_manual(values = top_pathway_colours) +
+    scale_fill_manual(
+      values = top_pathway_colours,
+      na.value = "white",
+      guide = NULL
+    ) +
+    scale_shape_manual(values = c("up" = 21), na.value = 16) +
+    labs(
+      size = "Bonferroni\np-value",
+      colour = "Pathway type"
+    ) +
+    theme_void(base_size = 18) +
+    theme(
+      legend.text.align = 0,
+      plot.margin = unit(rep(5, 4), "mm")
+    ) +
+    guides(
+      colour = guide_legend(override.aes = list(size = 5, pch = 19)),
+      size = guide_legend(override.aes = list(
+        colour = "black",
+        fill = "white",
+        stroke = 0.5
+      ))
+    )
 }
--- workflow_ex.R
+++ workflow_ex.R
@@ -1,19 +1,6 @@
 # Load packages & example data --------------------------------------------
 
-library(ggraph)
 devtools::load_all(".")
-
-
-top_pathway_colours <- set_names(
-  RColorBrewer::brewer.pal("Set2", n = 7),
-  c("Cell Process",
-    "Cell Replication",
-    "Signaling",
-    "Tissue Function",
-    "Immune and Hemostasis",
-    "Metabolism",
-    "Gene Expression")
-)
 
 
 # Example 1 ---------------------------------------------------------------
@@ -38,57 +25,18 @@ my_pathway_network <- create_pathnet(
   foundation = starting_pathways,
   trim = TRUE,
   trim_order = 1
-) %>%
-  mutate(node_fill = if_else(!is.na(description), grouped_pathway, NA_character_))
+)
 
 
 # |- plot -----------------------------------------------------------------
 
-ggraph(my_pathway_network, layout = "nicely") +
-  geom_edge_link(aes(edge_width = log10(similarity)), alpha = 0.67) +
-  geom_node_point(
-    aes(size = -log10(bonferroni), fill = node_fill, colour = grouped_pathway),
-    pch = 21,
-    stroke = 1.5
-  ) +
-  geom_node_label(
-    aes(label = pathway_name_1),
-    repel = TRUE,
-    alpha = 0.67,
-    min.segment.length = 0,
-    max.overlaps = 6
-  ) +
-  scale_edge_width(range = c(0.33, 3), name = "Similarity") +
-  scale_size_continuous(
-    labels = scales::label_math(expr = 10^-~.x),
-    range = c(4, 8)
-  ) +
-  scale_colour_manual(values = top_pathway_colours) +
-  scale_fill_manual(values = top_pathway_colours, na.value = "white", guide = NULL) +
-  scale_shape_manual(values = c("up" = 21), na.value = 16) +
-  labs(
-    size = "Bonferroni\np-value",
-    colour = "Pathway type"
-  ) +
-  theme_void(base_size = 18) +
-  theme(legend.text.align = 0) +
-  guides(
-    colour = guide_legend(override.aes = list(size = 6)),
-    size = guide_legend(override.aes = list(
-      pch = 21,
-      colour = "black",
-      fill = "white",
-      stroke = 0.5
-    ))
-  )
+plot_pathnet(my_pathway_network)
 
 
 # Example 2 ---------------------------------------------------------------
 
 # Here we'll restrict the data using the pathways/genes from the Sigora results,
-# instead of using all pathways and layering our results on top
-sigora_example_2
-
+# instead of using all pathways and layering our results on top.
 # Start by splitting the sigora genes, and adding the needed annotations from
 # sigora_database
 candidate_data <- sigora_example_2 %>%
@@ -120,40 +68,13 @@ candidates_as_network <- create_pathnet(
   trim = FALSE
 )
 
-ggraph(candidates_as_network, layout = "mds") +
+plot_pathnet(candidates_as_network) +
   facet_nodes(~direction, scales = "free", nrow = 2) +
-  geom_edge_link(aes(edge_width = log10(similarity)), alpha = 0.5) +
-  geom_node_point(
-    aes(size = -log10(bonferroni), fill = grouped_pathway),
-    pch = 21,
-    colour = "black"
-  ) +
-  geom_node_label(
-    aes(label = pathway_name_1),
-    size = 5,
-    repel = TRUE,
-    alpha = 0.7,
-    max.overlaps = 3,
-    min.segment.length = 0
-  ) +
-  scale_edge_width(range = c(0.5, 2), name = "Similarity") +
-  scale_size_continuous(
-    labels = scales::label_math(expr = 10^-~.x),
-    range = c(4, 8)
-  ) +
-  scale_fill_discrete(na.value = "grey") +
-  labs(
-    size = "Bonferroni\np-value",
-    fill = "Pathway type"
-  ) +
   theme_bw(base_size = 18) +
   theme(
     axis.title = element_blank(),
-    axis.text = element_blank(),
+    axis.text  = element_blank(),
     axis.ticks = element_blank(),
     panel.grid = element_blank(),
     legend.text.align = 0
-  ) +
-  guides(
-    fill = guide_legend(override.aes = list(size = 6))
   )
