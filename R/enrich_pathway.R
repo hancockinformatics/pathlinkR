f3fac43957c77ad5d4efa114199ff91cfc081a7e
Andy An <andy.an3559@gmail.com>
2023-05-08 18:38:35 -0700

    added functionality for ReactomePA

--- R/enrich_pathway.R
+++ R/enrich_pathway.R
@@ -14,6 +14,7 @@
 #' @export
 #'
 #' @import dplyr
+#' @import ReactomePA
 #'
 enrich_pathway <- function(
     deseq_result_list,
@@ -26,6 +27,9 @@ enrich_pathway <- function(
   # TODO
   # 1. How to make function continue to run if sigora fails?
   # 2. Include other pathway enrichment methods: hallmark, reactomePA
+  # 3. Include just using a vector of genes rather than a list of DESeq2 results
+  # 4. Should the candidate genes be in ensg_id or hgnc?
+  # 5. reactomePA uses pathway levels >4, so some pathways do not have annotated top pathways. Need to change pathway mapping file
 
   if (!is.list(deseq_result_list)) {
     message("Provide a list of dataframes of DESeq2 results with the name of ",
@@ -35,11 +39,24 @@ enrich_pathway <- function(
   } else {
     for (i in 1:length(deseq_result_list)) {
       comparison <- names(deseq_result_list[i])
+      message(paste0("Comparison being analyzed: ", comparison))
 
       # Use the DE genes
       deseq_results <- deseq_result_list[i][[1]] %>%
         filter(padj < p_cutoff, abs(log2FoldChange) > log2(fc_cutoff))
 
+      # If splitting into up and down-regulated DE genes
+      if (split) {
+        up_gns <- filter(deseq_results, log2FoldChange > 0) %>% rownames()
+        dn_gns <- filter(deseq_results, log2FoldChange < 0) %>% rownames()
+        message(paste0(
+          "DEGs used: Up: ", length(up_gns),
+          ", Down: ", length(dn_gns)
+        ))
+      } else {
+        all_gns <- rownames(deseq_results)
+        message(paste0("DEGs used: ", length(all_gns))) # Print how many DE genes used
+      }
 
       ### SIGORA analysis
       # SIGORA uses gene pairs with the Reactome database, which often
@@ -49,43 +66,141 @@ enrich_pathway <- function(
 
       if (analysis == "sigora") {
         message("Enrichment using SIGORA")
-        message(paste0("Comparison being analyzed: ", comparison))
 
-        # If up- and down-regulated genes are to be enriched separately
+        # Enrich with up- and down-regulated genes separately
         if (split) {
-          up_gns <- filter(deseq_results, log2FoldChange > 0) %>% rownames()
-          dn_gns <- filter(deseq_results, log2FoldChange < 0) %>% rownames()
-          message(paste0(
-            "DEGs used: Up: ",
-            length(up_gns),
-            ", Down: ", length(dn_gns)
-          ))
-
-          # run SIGORA using the internal function
           up_results <- run_sigora(up_gns, direction = "Up")
           dn_results <- run_sigora(dn_gns, direction = "Down")
-
           total_results <- rbind(up_results, dn_results)
 
         # Or just use all DE genes for enrichment
         } else {
-          de_gns <- rownames(deseq_results)
-          message(paste0("DEGs used: ", length(de_gns))) # Print how many DE genes used
-          total_results <- run_sigora(de_gns, direction = "All")
+          total_results <- run_sigora(all_gns, direction = "All")
+        }
+      }
+
+      ### ReactomePA analysis
+      # Reactome uses regular over-representation analysis
+      # Useful for finding more pathways, if you have fewer DE genes or pathways
+      # Less stringent than SIGORA enrichment
+
+      if(analysis == "reactomepa"){
+        # ReactomePA needs to map to entrez_ids, does not take ensembl ids
+        # can use SIGORA's mapping for consistency, but maps fewer entrez symbols
+        if (split) {
+          up_gns_entrez <- idmap %>%
+            filter(Ensembl.Gene.ID %in% up_gns) %>%
+            .$EntrezGene.ID %>%
+            unique()
+
+          up_results <- enrichPathway(up_gns_entrez, readable = TRUE) %>%
+            as.data.frame() %>%
+            mutate(direction = 'Up')
+
+          dn_gns_entrez <- idmap %>%
+            filter(Ensembl.Gene.ID %in% dn_gns) %>%
+            .$EntrezGene.ID %>%
+            unique()
+
+          dn_results <- enrichPathway(dn_gns_entrez, readable = TRUE) %>%
+            as.data.frame() %>%
+            mutate(direction = 'Down')
+
+          total_results <- rbind(up_results, dn_results)
+
+          # or use our own mapping file
+          # entrez_ids <- mapping_file %>%
+          #   filter(
+          #     ensg_id %in% up_gns
+          #   ) %>%
+          #   .$entrez_id %>%
+          #   unique()
+
+        } else {
+          all_gns_entrez <- idmap %>%
+            filter(Ensembl.Gene.ID %in% all_gns) %>%
+            .$EntrezGene.ID %>%
+            unique()
+
+          total_results <- enrichPathway(all_gns_entrez, readable = TRUE) %>%
+            as.data.frame() %>%
+            mutate(direction = 'All')
         }
 
-        total_results_annotated <- left_join(
-          total_results,
-          top_pathways %>% select(pathway_id, top_pathways),
-          by = "pathway_id"
-        )
-
-        total_results_annotated$comparison <- comparison
-        total_results_annotated$total_genes <- nrow(deseq_results)
-        message(paste0(
-          "Done! Enriched pathways: ", nrow(total_results_annotated)
-        ))
+        # clean up the column names
+        total_results <- total_results %>%
+          separate(GeneRatio,
+                   into = c('num_candidate_genes', 'num_bg_genes'),
+                   sep = '/') %>%
+          transmute(
+            pathway_id = ID,
+            pathway_description = Description,
+            direction,
+            p_value = pvalue,
+            p_value_adjusted = p.adjust,
+            q_value = qvalue,
+            candidate_genes = geneID,
+            num_candidate_genes = as.numeric(num_candidate_genes),
+            num_bg_genes = as.numeric(num_bg_genes),
+            gene_ratio = num_candidate_genes/num_bg_genes
+          ) %>%
+          # output genes are separated by '/', replace with ';' for consistency
+          mutate_at(.vars = 'candidate_genes',
+                    .funs = gsub,
+                    pattern = '/',
+                    replacement = ';'
+          )
       }
+
+      ### Hallmark
+      # if (analysis == "hallmark") {
+      #   database <- hallmark
+      #   msigdbr_t2g = database %>% dplyr::distinct(gs_name, ensembl_gene) %>% as.data.frame()
+      #
+      #   up <- dplyr::filter(dataframe, FoldChange > 0)
+      #   down <- dplyr::filter(dataframe, FoldChange < 0)
+      #
+      #   # Print how many DE genes
+      #   print(paste0("up: ", length(row.names(up)), ", down: ", length(row.names(down))))
+      #
+      #   up <- enricher(
+      #     gene = rownames(dataframe %>% filter(log2FoldChange > 0)),
+      #     TERM2GENE = msigdbr_t2g,
+      #     universe = universe
+      #   ) %>% as.data.frame()
+      #
+      #   if (length(rownames(up)) != 0) {
+      #     up$direction <- "Up"
+      #   }
+      #
+      #
+      #   down <- enricher(
+      #     gene = rownames(dataframe %>% filter(log2FoldChange < 0)),
+      #     TERM2GENE = msigdbr_t2g, universe = universe
+      #   ) %>% as.data.frame()
+      #
+      #   if (length(rownames(down)) != 0) {
+      #     down$direction <- "Down"
+      #   }
+      #
+      #   total <- rbind(up, down)
+      #   total$id <- filename
+      #   total$`-log10 p-adj`= -log10(total$p.adjust +1E-300)
+      #
+      # }
+
+      # annotate the  enrichment results by adding top pathway, comparisons
+      total_results_annotated <- left_join(
+        total_results,
+        top_pathways %>% select(pathway_id, top_pathways),
+        by = "pathway_id"
+      )
+
+      total_results_annotated$comparison <- comparison
+      total_results_annotated$total_genes <- nrow(deseq_results)
+      message(paste0(
+        "Done! Enriched pathways: ", nrow(total_results_annotated)
+      ))
 
       # This adds the next dataframe to the previous one
       if(i == 1){
@@ -97,88 +212,11 @@ enrich_pathway <- function(
     }
   }
 
+  # order the comparisons in the same order as entered (for graphing purposes later)
   final_enriched_results$comparison <- factor(
     final_enriched_results$comparison,
     levels = c(names(deseq_result_list))
   )
-
-  ### ReactomePA
-  # if(analysis == "reactomePA"){
-  #   mapping <- final_map_condense %>% column_to_rownames(var = "ensg_id")
-  #   dataframe <- merge(dataframe, mapping, by = 0, all.x = TRUE)
-  #
-  #   up <- dplyr::filter(dataframe, FoldChange > 0)
-  #   down <- dplyr::filter(dataframe, FoldChange < 0)
-  #   # Print how many DE genes
-  #   print(paste0("up: ", length(row.names(up)), ", down: ", length(row.names(down))))
-  #
-  #   up <- enrichPathway(up$entrez_id, readable = TRUE) %>% as.data.frame()
-  #   if(length(row.names(up) > 0)){
-  #     up$id=filename
-  #     up$direction="Up"
-  #   }
-  #   down <- enrichPathway(down$entrez_id, readable = TRUE) %>% as.data.frame()
-  #   if(length(row.names(down) > 0)){
-  #     down$id=filename
-  #     down$direction="Down"
-  #   }
-  #   total <- rbind(up, down)
-  #   total$`-log10 p-adj`=(-log((total$p.adjust+1E-300), 10))
-  #   names(total)[1] <- "pathwy.id"
-  #   names(total)[2] <- "description"
-  #
-  #   # Annotate the "top" pathways
-  #   # Generates three new columns to fill
-  #   total[, c("Top Pathway Name", "Hierarchy")] <- NA
-  #
-  #   # Fills in column per row based on the first column (pathway id)
-  #   for (i in c(1:length(row.names(total)))) {
-  #     total[i , c("Top Pathway Name", "Hierarchy")] <- path_steps_local(total[i,1])
-  #     print(i)
-  #   }
-  #
-  #   # Save as a table
-  #   write.table(total, paste0("react_pathways_",filename,".csv"), sep = ",", row.names=FALSE)
-  #
-  # }
-
-
-  ### Hallmark
-  # if (analysis == "hallmark") {
-  #   database <- hallmark
-  #   msigdbr_t2g = database %>% dplyr::distinct(gs_name, ensembl_gene) %>% as.data.frame()
-  #
-  #   up <- dplyr::filter(dataframe, FoldChange > 0)
-  #   down <- dplyr::filter(dataframe, FoldChange < 0)
-  #
-  #   # Print how many DE genes
-  #   print(paste0("up: ", length(row.names(up)), ", down: ", length(row.names(down))))
-  #
-  #   up <- enricher(
-  #     gene = rownames(dataframe %>% filter(log2FoldChange > 0)),
-  #     TERM2GENE = msigdbr_t2g,
-  #     universe = universe
-  #   ) %>% as.data.frame()
-  #
-  #   if (length(rownames(up)) != 0) {
-  #     up$direction <- "Up"
-  #   }
-  #
-  #
-  #   down <- enricher(
-  #     gene = rownames(dataframe %>% filter(log2FoldChange < 0)),
-  #     TERM2GENE = msigdbr_t2g, universe = universe
-  #   ) %>% as.data.frame()
-  #
-  #   if (length(rownames(down)) != 0) {
-  #     down$direction <- "Down"
-  #   }
-  #
-  #   total <- rbind(up, down)
-  #   total$id <- filename
-  #   total$`-log10 p-adj`= -log10(total$p.adjust +1E-300)
-  #
-  # }
 
   # Return the results, which is a tibble of all the pathway results
   return(as_tibble(final_enriched_results))
