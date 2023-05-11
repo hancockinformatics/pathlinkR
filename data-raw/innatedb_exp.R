cde763655128339fb584ed7a11ce4716a08e9a45
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-05-11 11:30:04 -0700

    Changes to format and compression of some data objects. Removed unused script from data-raw.

--- data-raw/innatedb_exp.R
+++ data-raw/innatedb_exp.R
@@ -1,26 +1,27 @@
-
 # Load packages
+library(biomaRt)
 library(dplyr)
 
 
 # Load and filter InnateDB data
-innatedb_init <- readr::read_tsv(
-  file = "https://innatedb.com/download/interactions/all.mitab.gz",
-  col_types = cols()
-)
+innatedb_init <-
+  readr::read_tsv("https://innatedb.com/download/interactions/all.mitab.gz")
 
-innatedb_filtered <- innatedb_init %>%
-  filter(
-    # We just want human interactions
-    ncbi_taxid_A == "taxid:9606(Human)" & ncbi_taxid_B == "taxid:9606(Human)"
-  )
+
+# We just want human interactions
+innatedb_filtered <- innatedb_init %>% filter(
+  ncbi_taxid_A == "taxid:9606(Human)" & ncbi_taxid_B == "taxid:9606(Human)"
+)
 
 innatedb_trimmed <- innatedb_filtered %>%
   select(
     "ensembl_gene_A" = alt_identifier_A,
     "ensembl_gene_B" = alt_identifier_B
   ) %>%
-  mutate(across(everything(), stringr::str_remove, pattern = "ensembl\\:")) %>%
+  mutate(across(
+    everything(),
+    ~stringr::str_remove(.x, pattern = "ensembl\\:")
+  )) %>%
   distinct(ensembl_gene_A, ensembl_gene_B)
 
 
@@ -31,16 +32,23 @@ innatedb_no_dups <- innatedb_trimmed[
 
 
 # Get gene mapping from biomaRt
-biomart_mapping <- biomaRt::getBM(
-  mart       = biomaRt::useMart("ensembl", dataset = "hsapiens_gene_ensembl"),
-  attributes = c("ensembl_gene_id", "hgnc_symbol"),
-  verbose    = TRUE
+biomart_mapping <- getBM(
+  mart = useMart("ensembl", dataset = "hsapiens_gene_ensembl"),
+  attributes = c("ensembl_gene_id", "hgnc_symbol")
 )
 
 innatedb_mapped <- innatedb_no_dups %>%
-  left_join(biomart_mapping, by = c("ensembl_gene_A" = "ensembl_gene_id")) %>%
+  left_join(
+    biomart_mapping,
+    by = c("ensembl_gene_A" = "ensembl_gene_id"),
+    multiple = "all"
+  ) %>%
   rename("hgnc_symbol_A" = hgnc_symbol) %>%
-  left_join(biomart_mapping, by = c("ensembl_gene_B" = "ensembl_gene_id")) %>%
+  left_join(
+    biomart_mapping,
+    by = c("ensembl_gene_B" = "ensembl_gene_id"),
+    multiple = "all"
+  ) %>%
   rename("hgnc_symbol_B" = hgnc_symbol) %>%
   relocate(ends_with("A"))
 
@@ -55,9 +63,5 @@ innatedb_exp <- innatedb_mapped %>%
   ungroup()
 
 
-# Get a quick summary of the top interactors
-innatedb_exp %>% count(hgnc_symbol_A) %>% arrange(desc(n))
-innatedb_exp %>% count(hgnc_symbol_B) %>% arrange(desc(n))
-
-
-usethis::use_data(innatedb_exp, overwrite = TRUE)
+# Save the data, being sure to enable compression to reduce the size
+usethis::use_data(innatedb_exp, overwrite = TRUE, compress = "bzip2")
