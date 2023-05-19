ec3221794def245222b56a54b729df30ee60cc99
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-05-19 11:16:32 -0700

    Modified run_sigora to not use a for loop for better speed.

--- R/run_sigora.R
+++ R/run_sigora.R
@@ -8,9 +8,10 @@
 #'
 #' @import dplyr
 #' @import sigora
+#' @importFrom purrr map imap
 #'
-#' @description
-#' A short description...
+#' @description Internal wrapper function to run Sigora and return the results
+#'   with desired columns
 #'
 #' @references None.
 #'
@@ -22,8 +23,8 @@ run_sigora <- function(
     gps_repo
 ) {
 
-  # Run SIGORA based on default settings (GPSrepo = reaH, levels = 4, adjusted
-  # pvalue cutoff < 0.001)
+  # Run SIGORA based on default settings (GPSrepo = reaH, level = 4)
+  message("\t=> Call 'sigora()' for ", direction, "...")
   invisible(capture.output(
     sigora_data <- sigora(
       GPSrepo = gps_repo,
@@ -36,16 +37,16 @@ run_sigora <- function(
   sigora_results <- sigora_data$summary_results
 
 
-  # Not all DE genes are used in the Reactome pathway genes from SIGORA. SIGORA
+  # Not all DE genes are used in the Reactome pathway genes from Sigora. Sigora
   # does not have the functionality to calculate gene ratio, which might be
   # useful, but it is basically a reflection of p-value and pathway size. The
   # gene ratio is somewhat confusing to calculate. Here, gene ratio = k/n where:
   #   k = analyzed genes (the genes in enrich_genes) that are found in the
   #       pathway of interest
   #   n = analyzed genes that are found in the entire database (all genes used
-  #       in SIGORA in the Reactome database)
+  #       in Sigora in the Reactome database)
 
-  # Calculate n
+  # Start by calculating n
   n_genes <- enrich_genes[enrich_genes %in% idmap$Ensembl.Gene.ID] %>% length()
 
   if (nrow(sigora_results) > 0) {
@@ -53,80 +54,48 @@ run_sigora <- function(
     sigora_results$direction <- direction
 
     # Get the DE genes that were enriched for each pathway
-    for (i in 1:length(sigora_results$pathwy.id)) {
+    message("\t=> Get gene ratio data...")
 
-      pathid <- sigora_results$pathwy.id[i]
+    sigora_detailed_list_1 <- sigora_data$detailed_results %>%
+      as_tibble() %>%
+      select(pathway, contains("gene")) %>%
+      split(x = ., f = .$pathway)
 
-      # First, get the enriched gene pairs for that pathway
-      geneid <- sigora_data$detailed_results %>%
-        as.data.frame() %>%
-        filter(pathway == pathid)
-
-      # The unique Entrez IDs from the gene pairs
-      unique_entrez <- unique(c(geneid$gene1, geneid$gene2))
-
-      # Convert them into HGNC symbols using SIGORA's internal mapping file,
-      # keeping the genes that were input. There may be some discrepancies
-      # between the number of unique Entrez IDs HGNC symbols due to duplicate
-      # mapping. Also, use the ones that also were in the original ENSG input.
-
-      unique_hgnc <- unique(
-        idmap %>%
-          filter(
-            EntrezGene.ID %in% unique_entrez & Ensembl.Gene.ID %in% enrich_genes
-          ) %>%
-          .$Symbol %>%
-          as.character()
-      )
-
-      # remove any that did not map to a gene symbol
-      unique_hgnc <- unique_hgnc[unique_hgnc != ""]
-
-      # Old code that converted the entrez IDs to ENSG IDs, may implement
-      # unique_ensg <- unique(
-      #   idmap %>%
-      #     filter(
-      #       EntrezGene.ID %in% unique_entrez & Ensembl.Gene.ID %in% enrich_genes
-      #     ) %>%
-      #     .$Ensembl.Gene.ID %>%
-      #     as.character()
-      # )
-
-      # Or, use our own mapping file for conversion
-      # unique_ensg <- unique(
-      #   mapping_file %>% filter(
-      #     entrez_id %in% unique_entrez &
-      #       ensg_id %in% enrich_genes
-      #   ) %>% .$ensg_id
-      # )
-      #
-
-      pathway_info_df <- data.frame(
-        pathwy.id = pathid,
-        genes = paste(unique_hgnc, collapse = ';'),
-        num_candidate_genes = length(unique_hgnc)
+    sigora_detailed_list_2 <- sigora_detailed_list_1 %>% imap(
+      ~tibble(
+        pathwy.id = .y,
+        EntrezGene.ID = unique(c(pull(.x, gene1), pull(.x, gene2)))
       ) %>%
-        mutate(gene_ratio = num_candidate_genes/n_genes)
+        left_join(idmap, by = "EntrezGene.ID") %>%
+        filter(Ensembl.Gene.ID %in% enrich_genes, Symbol != "^$") %>%
+        select(pathwy.id, Symbol) %>%
+        mutate(across(everything(), as.character)) %>%
+        distinct() %>%
+        arrange(pathwy.id, Symbol)
+    ) %>%
+      bind_rows() %>%
+      group_by(pathwy.id) %>%
+      summarize(genes = paste0(Symbol, collapse = ";")) %>%
+      mutate(num_candidate_genes = 1 + str_count(genes, ";")) %>%
+      ungroup() %>%
+      mutate(gene_ratio = num_candidate_genes / n_genes)
 
-      if (i == 1) {
-        final_results <- pathway_info_df
-      } else {
-        final_results <- rbind(final_results, pathway_info_df)
-      }
-    }
+    final_results <- sigora_detailed_list_2
   }
 
+  message("\t=> Cleaning output...")
   final_output <-
     left_join(sigora_results, final_results, by = "pathwy.id") %>%
-    transmute(
-      pathway_id = pathwy.id,
-      pathway_description = description,
+    mutate(num_bg_genes = n_genes) %>%
+    select(
+      "pathway_id" = pathwy.id,
+      "pathway_description" = description,
       direction,
-      p_value = pvalues,
-      p_value_adjusted = Bonferroni,
+      "p_value" = pvalues,
+      "p_value_adjusted" = Bonferroni,
       genes,
       num_candidate_genes,
-      num_bg_genes = n_genes,
+      num_bg_genes,
       gene_ratio
     )
 }
