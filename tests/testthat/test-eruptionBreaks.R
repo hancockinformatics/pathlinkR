ebcdf24263081007aedbe336b8890878da44e1dd
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-07-21 11:37:22 -0700

    Fixed or expanded tests for multiple functions.

--- tests/testthat/test-eruptionBreaks.R
+++ tests/testthat/test-eruptionBreaks.R
@@ -1,3 +1,16 @@
 test_that("breaks are of the correct class", {
-  expect_type(.eruptionBreaks(c(4, 5)), "environment")
+    expect_type(.eruptionBreaks(c(-5, 5)), "environment")
+    expect_type(.eruptionBreaks(c(-6.5, 6.5)), "environment")
+    expect_type(.eruptionBreaks(c(-9, 9)), "environment")
+    expect_type(.eruptionBreaks(c(-13, 13)), "environment")
+    expect_type(.eruptionBreaks(c(-16, 16)), "environment")
+})
+
+test_that("the correct message is returned", {
+    expect_message(
+        .eruptionBreaks(c(-20, 20)),
+        regexp = paste0(
+            "Something may be wrong with your DESeq model"
+        )
+    )
 })
--- tests/testthat/test-plotPathways.R
+++ tests/testthat/test-plotPathways.R
@@ -1,9 +1,8 @@
+set.seed(1)
 exPathwayPlot <- plotPathways(
     sigoraExamples,
     columns = 2
 )
-
-set.seed(1)
 
 test_that("pathway plots are correct", {
     vdiffr::expect_doppelganger("pathwayPlotExample", exPathwayPlot)
