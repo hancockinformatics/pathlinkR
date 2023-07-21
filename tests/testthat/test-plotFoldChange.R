42a686e67861aa02b66733c4fb5e8482cb3c0c3c
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-07-21 11:37:22 -0700

    Fixed or expanded tests for multiple functions.
    Former-commit-id: cbb3f98938ca7e908b11cac4a1213b4e93151d95
    Former-commit-id: f657d771bc51097b7b73e1630d3b722dbcb0fce2

--- tests/testthat/test-plotFoldChange.R
+++ tests/testthat/test-plotFoldChange.R
@@ -1,9 +1,8 @@
+set.seed(1)
 exPlotFoldChange <- plotFoldChange(
     deseqExampleList,
     pathName = "Generation of second messenger molecules"
 )
-
-set.seed(1)
 
 test_that("fold change heatmaps are working", {
     vdiffr::expect_doppelganger("plotFoldChangeExample", exPlotFoldChange)
