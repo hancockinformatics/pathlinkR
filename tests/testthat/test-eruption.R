e7a113c122d8e5aac5f2bbe89923b2991ebc73ab
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-08-31 09:43:23 -0700

    Updated tests for eruption for better coverage.
    Former-commit-id: 7037e5598dee679f6722b2abc0a08bf01a39b9c8
    Former-commit-id: 0d6ef5573894a91de91f6b7a3b50fd417ccb4bb5

--- tests/testthat/test-eruption.R
+++ tests/testthat/test-eruption.R
@@ -1,8 +1,26 @@
-test_that("the volcano plot is correct", {
+test_that("a basic volcano plot is correct", {
     set.seed(1)
 
     vdiffr::expect_doppelganger(
         "volcanoPlotExample",
         eruption(deseqResult=deseqExampleList[[1]])
+    )
+})
+
+test_that("some of the options are working as expected", {
+    set.seed(1)
+
+    interferonGenes <- sigoraDatabase %>%
+        filter(pathwayName == "Interferon Signaling") %>%
+        pull(ensemblGeneId)
+
+    vdiffr::expect_doppelganger(
+        "volcanoPlotExample2",
+        eruption(
+            deseqResult=deseqExampleList[[1]],
+            xaxis=c(-4, 4),
+            yaxis=c(0, 8),
+            highlightGenes=interferonGenes
+        )
     )
 })
