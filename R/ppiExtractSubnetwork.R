64f2bc5e5ab9c7fef464ab1734efd3dca124e7d7
Travis Blimkie <travis.m.blimkie@gmail.com>
2024-02-16 13:55:22 -0800

    Fixed igraph functions that had been deprecated.

--- R/ppiExtractSubnetwork.R
+++ R/ppiExtractSubnetwork.R
@@ -30,8 +30,8 @@
 #'
 #' @import dplyr
 #'
-#' @importFrom igraph as.igraph decompose.graph delete.vertices
-#'   get.shortest.paths induced.subgraph simplify V
+#' @importFrom igraph as.igraph decompose delete_vertices
+#'   shortest_paths induced_subgraph simplify V
 #' @importFrom tidygraph as_tbl_graph
 #'
 #' @details Uses functions from the igraph package to extract a minimally
@@ -148,13 +148,13 @@ ppiExtractSubnetwork <- function(
 
     # Get subgraphs, which will only contain the specified nodes
     message("Calculating subgraphs from specified nodes...")
-    allSubgraphs <- induced.subgraph(
+    allSubgraphs <- induced_subgraph(
         graph=as.igraph(network),
         vids=geneNodeIds
     )
 
     # Decompose each subgraph
-    allComponents <- decompose.graph(graph=allSubgraphs, min.vertices=1)
+    allComponents <- decompose(graph=allSubgraphs, min.vertices=1)
 
     # If all the specified nodes form a single, connected network, pull that...
     if (length(allComponents) == 1) {
@@ -168,7 +168,7 @@ ppiExtractSubnetwork <- function(
         moduleShortestPaths <- list()
 
         for (i in seq(length(geneNodeIds))) {
-            moduleShortestPaths[[i]] <- get.shortest.paths(
+            moduleShortestPaths[[i]] <- shortest_paths(
                 as.igraph(network),
                 geneNodeIds[i],
                 geneNodeIds[-seq(i)]
@@ -177,7 +177,7 @@ ppiExtractSubnetwork <- function(
 
         moduleNodes <- unique(unlist(moduleShortestPaths))
         NodesToRemove <- igraph::V(network)$name[-moduleNodes]
-        moduleNetwork <- simplify(delete.vertices(network, NodesToRemove))
+        moduleNetwork <- simplify(delete_vertices(network, NodesToRemove))
     }
 
     moduleNetworkTidygraph <- as_tbl_graph(moduleNetwork)
