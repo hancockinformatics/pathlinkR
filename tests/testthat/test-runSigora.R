ddb7b4713571eddd63e020c2cee33818b77e4c0e
Travis Blimkie <travis.m.blimkie@gmail.com>
2024-02-23 09:18:26 -0800

    Moving github pages pathlinkR vignette to a branch of the main respository.

--- tests/testthat/test-runSigora.R
+++
@@ -1,6 +0,0 @@
-test_that("runSigora works on a basic level", {
-    data("exampleDESeqResults")
-
-    testGenes <- rownames(exampleDESeqResults[[1]])[seq(500)]
-    expect_no_error(.runSigora(testGenes, gpsRepo="default"))
-})
--- tests/testthat/test-truncNeatly.R
+++
@@ -1,7 +0,0 @@
-test_that("string truncation works", {
-  expect_equal(.truncNeatly("This is a test string", 18), "This is a test...")
-})
-
-test_that("NA in means NA out", {
-    expect_equal(.truncNeatly(x=NA), NA_character_)
-})
