cc4381045d86f1a693b1363c7f5c6765b4158893
Travis Blimkie <travis.m.blimkie@gmail.com>
2026-08-20 14:27:20 -0700

    Added mouse test for .runSigora

--- tests/testthat/test-runSigora.R
+++ tests/testthat/test-runSigora.R
@@ -6,3 +6,13 @@ test_that("runSigora works on a basic level", {
 
     expect_equal(dim(testResult), c(331, 8))
 })
+
+test_that("runSigora works on a basic level", {
+    data("exampleDESeqResultsMM")
+
+    testGenes <- rownames(exampleDESeqResultsMM[[1]])[seq(500)]
+    testResult <- .runSigora(testGenes, gpsRepo="reaM", gpsLevel=4)
+
+    expect_equal(dim(testResult), c(280, 8))
+})
+
